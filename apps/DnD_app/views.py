36dd3a089712493821fc65e4e6cff02580b8d672
Chanto Umana <umanav@amazon.com>
2018-04-17 15:45:20 -0600

    second day update. Adjustments to registration and added new route of save, updated routes already set previously

--- apps/DnD_app/views.py
+++ apps/DnD_app/views.py
@@ -9,19 +9,19 @@ def index(request):
     return redirect("/profile")
 
 def register(request):
-    result = User.objects.validate_reg(request.POST)
-    if result[0]:
-        request.session['user_id'] = result[1].id
-        request.session['user_name'] = result[1].name
-        request.session['email'] = result[1].email
-        return redirect("DnD_app/profile"))
+    result = User.objects.valid_registration(request.POST)
+    if result[1] == False:
+        request.session['id'] = result[0].id
+        request.session['user_name'] = result[0].username
+        return redirect("/profile")
     else:
-        for error in result[1]: 
-            messages.add_message(request, messages.INFO, error)
-    return redirect(request, "/DnD_app/index")
+        for error in result[0]: 
+            messages.error(request, error)
+    return redirect("/")
 
 def profile(request):
-    return render(request.'DnD_app/profile.hhml/)
+    user=User.objects.get(id=request.session['id'])
+    return render(request,'DnD_app/profile.html')
 
 def login(request):
     errors = User.objects.valid_login(request.POST)
@@ -30,18 +30,45 @@ def login(request):
             messages.error(request,error)
         return redirect ('/')
     else:
-        request.session['user'] = User.objects.get(username=request.POST['username']).username
+        request.session['user_name'] = User.objects.get(username=request.POST['username']).username
         request.session['id'] = User.objects.get(username=request.POST['username']).id
         return redirect ('/profile')
 
+def logout(request):
+    del request.session['id']
+    return redirect('/')
+
 def new_Game(request):
-    return render(request, "DnD_app/character.html")
+    characters = Character.objects.all()
+    return render(request, "DnD_app/character.html",{'characters':characters})
+
+def save(request):
+    active = Game.objects.active_game(request.session['id'])
+    print active
+    if active[0] == False:
+        user= User.objects.get(id=request.session['id'])
+        game= Game.objects.create(user = user, hp = request.session['hp'], gold=request.session['gold'], level=request.session['level'])
+    else:
+        for active_game in active[1]: 
+            game = Game.objects.get(id=active_game.id)
+            game.hp = request.session['hp']
+            game.gold = request.session['gold']
+            game.level = request.session['level']
+    return redirect('/profile')
 
 def restart(request):
+    del request.session['level']
+    current = Game.objects.get(user_id=request.session['id'])
+    print current
+    current.delete()
     return redirect ('/new_Game')
 
 def keep_playing(request):
     return render(request, "DnD_app/game.html")
 
-def game(request):
+def game(request, id):
+    character = Character.objects.get(id=id)
+    request.session['hp'] = character.hp
+    request.session['gold'] = character.gold
+    request.session['level'] = 1
     return render(request, "DnD_app/game.html")
